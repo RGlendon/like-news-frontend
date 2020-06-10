41befb915c357133abaf5b792ee46f753df6141c
RGlendon <Rold.Glendon@gmail.com>
2020-06-10 09:52:30 +0300

    update search-block and header, added button

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ import './style.css';
 import Menu from '../scripts/menu';
 
 new Menu(document.querySelector('.menu'));
+
+document.addEventListener('click', (e) => {
+  if (e.target.matches('.button')) {
+    e.target.classList.add('button_active');
+    setTimeout(() => e.target.classList.remove('button_active'), 200);
+  }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log('hello')
+});
